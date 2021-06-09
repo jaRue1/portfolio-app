71a7b4799150924c5db12537f5c9d7f4be69ca58
jaRue1 <jaruejohnson@outlook.com>
2021-06-08 23:53:18 -0400

    added popup frame

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,7 +5,24 @@ import cityGuide from '../images/city-guide-app.png'
 import taskManager from '../images/task-manager.png'
 // Font awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
-import { faSearchMinus, faSearchPlus } from '@fortawesome/free-solid-svg-icons';
+import { faSearchPlus } from '@fortawesome/free-solid-svg-icons';
+
+import {PopupboxManager, PopupboxContainer} from "react-popupbox";
+
+
+// Project Linkx
+const openPopupboxLinkx = () => {
+  const content = (
+  <>
+  <img src={netflix} alt="linkx"/>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ipsum necessitatibus ratione praesentium </p>
+  <b>Demo</b><a className="hyper-link" onClick= {() => window.open("http://majorlinkx.io-app.s3-website-us-east-1.amazonaws.com/")}>http://majorlinkx.io-app.s3-website-us-east-1.amazonaws.com/</a>
+  </>
+  )
+  PopupboxManager.open({content})
+  
+} 
+//  popupboxConfigLinkx ={}
 
 function Portfolio() {
   return (
@@ -14,7 +31,7 @@ function Portfolio() {
        <div className="h1 text-uppercase text-center py-5">portfolio</div>
        <div className="image-box-wrapper">
          
-        <div className="portfolio-image-box">
+        <div className="portfolio-image-box" onClick={openPopupboxLinkx}>
           <img className="portfolio-images" src={netflix} alt="Desktop app" />
           <div className="overflow"></div>
           <FontAwesomeIcon className='portfolio-icon' icon={faSearchPlus}/>
@@ -45,6 +62,7 @@ function Portfolio() {
         </div>
        </div>
      </div>
+     {/* <PopupboxContainer {...popupboxConfigLinkx}/> */}
    </div>
   )
 }
