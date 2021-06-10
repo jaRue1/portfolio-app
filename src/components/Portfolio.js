92554f217206b5f3458f907df98c702ce48398e6
jaRue1 <jaruejohnson@outlook.com>
2021-06-10 00:43:47 -0400

    finished portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,23 +8,100 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearchPlus } from '@fortawesome/free-solid-svg-icons';
 
 import {PopupboxManager, PopupboxContainer} from "react-popupbox";
+import "react-popupbox/dist/react-popupbox.css"
 
 
-// Project Linkx
+
+
+function Portfolio() {
+
+  // Project Linkx
 const openPopupboxLinkx = () => {
   const content = (
   <>
-  <img src={netflix} alt="linkx"/>
+  <h2>Majorlinkx.io project</h2>
+  <img className="portfolio-image-popupbox" src={netflix} alt="linkx"/>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ipsum necessitatibus ratione praesentium </p>
-  <b>Demo</b><a className="hyper-link" onClick= {() => window.open("http://majorlinkx.io-app.s3-website-us-east-1.amazonaws.com/")}>http://majorlinkx.io-app.s3-website-us-east-1.amazonaws.com/</a>
+  <b>Demo: </b><a className="hyper-link" onClick= {() => window.open("http://majorlinkx.io-app.s3-website-us-east-1.amazonaws.com/")}>Continue</a>
   </>
   )
   PopupboxManager.open({content})
   
-} 
-//  popupboxConfigLinkx ={}
+}
+const popupboxConfigLinkx = {
+    titleBar: {
+      enable: false,
+      text: ""
+    },
+    fadeIn: true,
+    fadeInSpeed: 500,
+ } 
 
-function Portfolio() {
+
+   // Restaurants web app
+const openPopupboxRestaurants = () => {
+  const content = (
+  <>
+  <img className="portfolio-image-popupbox" src={cityGuide} alt="Restaurants"/>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ipsum necessitatibus ratione praesentium </p>
+  <b>Github: </b><a className="hyper-link" onClick= {() => window.open("https://github.com/jaRue1")}>Learn More</a>
+  </>
+  )
+  PopupboxManager.open({content})
+  
+}
+const popupboxConfigRestaurants = {
+    titleBar: {
+      enable: false,
+      text: ""
+    },
+    fadeIn: true,
+    fadeInSpeed: 500,
+ } 
+
+    // Portfolio web app
+const openPopupboxPortfolio = () => {
+  const content = (
+  <>
+  <img className="portfolio-image-popupbox" src={portfolio} alt="Portfolio"/>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ipsum necessitatibus ratione praesentium </p>
+  <b>Github: </b><a className="hyper-link" onClick= {() => window.open("https://github.com/jaRue1")}>Learn More</a>
+  </>
+  )
+  PopupboxManager.open({content})
+  
+}
+const popupboxConfigPortfolio = {
+    titleBar: {
+      enable: false,
+      text: ""
+    },
+    fadeIn: true,
+    fadeInSpeed: 500,
+ } 
+
+    // Desktop web app
+    const openPopupboxDesktop = () => {
+      const content = (
+      <>
+      <img className="portfolio-image-popupbox" src={taskManager} alt="Desktop"/>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ipsum necessitatibus ratione praesentium </p>
+      <b>Github: </b><a className="hyper-link" onClick= {() => window.open("https://github.com/jaRue1")}>Learn More</a>
+      </>
+      )
+      PopupboxManager.open({content})
+      
+    }
+    const popupboxConfigDesktop = {
+        titleBar: {
+          enable: true,
+          text: "My Applications"
+        },
+        fadeIn: true,
+        fadeInSpeed: 500,
+     } 
+
+
   return (
    <div className="portfolio-wrapper">
      <div className="container">
@@ -32,14 +109,14 @@ function Portfolio() {
        <div className="image-box-wrapper">
          
         <div className="portfolio-image-box" onClick={openPopupboxLinkx}>
-          <img className="portfolio-images" src={netflix} alt="Desktop app" />
+          <img className="portfolio-images" src={netflix} alt=" majorlinkx web app" />
           <div className="overflow"></div>
           <FontAwesomeIcon className='portfolio-icon' icon={faSearchPlus}/>
         </div>
        
        {/*  */}
       
-        <div className="portfolio-image-box">
+        <div className="portfolio-image-box"onClick={openPopupboxRestaurants}>
          <img className="portfolio-images" src={cityGuide} alt="restaurant web app" />
          <div className="overflow"></div>
          <FontAwesomeIcon className='portfolio-icon' icon={faSearchPlus}/>
@@ -47,7 +124,7 @@ function Portfolio() {
        
        {/*  */}
        
-        <div className="portfolio-image-box">
+        <div className="portfolio-image-box" onClick={openPopupboxPortfolio} >
          <img className="portfolio-images" src={portfolio} alt="portolio web app" />
          <div className="overflow"></div>
          <FontAwesomeIcon className='portfolio-icon' icon={faSearchPlus}/>
@@ -55,14 +132,17 @@ function Portfolio() {
     
        {/*  */}
       
-        <div className="portfolio-image-box">
-         <img className="portfolio-images" src={taskManager} alt="majorlinkx web app" />
+        <div className="portfolio-image-box" onClick={openPopupboxDesktop}>
+         <img className="portfolio-images" src={taskManager} alt="Desktop app" />
          <div className="overflow"></div>
          <FontAwesomeIcon className='portfolio-icon' icon={faSearchPlus}/>
         </div>
        </div>
      </div>
-     {/* <PopupboxContainer {...popupboxConfigLinkx}/> */}
+     <PopupboxContainer {...popupboxConfigLinkx}/>
+     <PopupboxContainer {...popupboxConfigRestaurants}/>
+     <PopupboxContainer {...popupboxConfigPortfolio}/>
+     <PopupboxContainer {...popupboxConfigDesktop}/>
    </div>
   )
 }
